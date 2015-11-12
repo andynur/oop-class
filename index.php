21bfaa80c35796579a0b854924277a1db186e355
Andy Nur <andylulz@yahoo.com>
2015-11-12 16:10:31 +0700

    On branch master
    Initial commit
    
     Changes to be committed:
    	new file:   index.php
    
     Untracked files:
    	oop-class/

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Belajar ngeGit kk</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1>Ini adalah basic penggunaan git.</h1>
+	<ul>
+		<li>Ini List 1</li>
+	</ul>
+</body>
+</html>
